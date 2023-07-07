0e46097a0e66a0ddcf609c70ff6775a1332d2f08
Jeymar Herrera <108249893+jeymarherrera@users.noreply.github.com>
2023-07-06 23:48:04 -0500

    blances general

---
+++ resources/js/src/views/balance.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="layout-px-spacing dash_2">
+        <teleport to="#breadcrumb">
+            <ul class="navbar-nav flex-row">
+                <li>
+                    <div class="page-header">
+                        <nav class="breadcrumb-one" aria-label="breadcrumb">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="javascript:;">Balance General</a></li>
+                            </ol>
+                        </nav>
+                    </div>
+                </li>
+            </ul>
+        </teleport>
+
+        <div class="row layout-top-spacing">
+
+            <div class="col-xl-4 col-lg-6 col-md-6 col-sm-12 col-12 layout-spacing">
+                <div class="widget widget-card-two">
+                    <div class="widget-heading">
+                        <div class="media">
+                            <div class="media-aside align-self-start">
+                                <div class="w-img">
+                                    <img src="@/assets/images/g-8.png" alt="avatar" />
+                                </div>
+                            </div>
+                            <div class="media-body">
+                                <h6>Salsas Camille - Balance General</h6>
+                                <p class="meta-date-time">del 1 de Enero al 31 de Diciembre de 2022</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="widget-content">
+                        <h5 style="color:red;">Año Anterior</h5>
+                        <a href="/balance_detalles" class="btn btn-secondary">Ver Detalles</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-xl-4 col-lg-6 col-md-6 col-sm-12 col-12 layout-spacing">
+                <div class="widget widget-card-two">
+                    <div class="widget-heading">
+                        <div class="media">
+                            <div class="media-aside align-self-start">
+                                <div class="w-img">
+                                    <img src="@/assets/images/g-8.png" alt="avatar" />
+                                </div>
+                            </div>
+                            <div class="media-body">
+                                <h6>Salsas Camille - Balance General</h6>
+                                <p class="meta-date-time">del 1 de Enero al 31 de Diciembre de 2023</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="widget-content">
+                        <h5>Año Actual</h5>
+                        <a href="javascript:;" class="btn btn-secondary">Ver Detalles</a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</template>
+<script setup>
+    import '@/assets/sass/widgets/widgets.scss';
+    import { computed, ref } from 'vue';
+    import { useStore } from 'vuex';
+
+
+    const store = useStore();
+
+</script>
---
+++ resources/js/src/views/balance_detalles.vue
@@ -0,0 +1,204 @@
+<template>
+    <div class="layout-px-spacing">
+        <teleport to="#breadcrumb">
+            <ul class="navbar-nav flex-row">
+                <li>
+                    <div class="page-header">
+                        <nav class="breadcrumb-one" aria-label="breadcrumb">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="javascript:;">Balance General</a></li>
+                                <li class="breadcrumb-item active" aria-current="page"><span></span></li>
+                            </ol>
+                        </nav>
+                    </div>
+                </li>
+            </ul>
+        </teleport>
+
+        <div class="row invoice layout-top-spacing layout-spacing apps-invoice">
+            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                <div class="doc-container">
+                    <div class="row">
+                        <div class="col-xl-9">
+                            <div class="invoice-container">
+                                <div class="invoice-inbox">
+                                    <div id="ct" class="">
+                                        <div class="invoice-00001">
+                                            <div class="content-section">
+                                                <!-- aqui -->
+                                                <div class="inv--head-section inv--detail-section">
+                                                    <div class="row">
+                                                        <div class="col-sm-6 col-12 me-auto">
+                                                            <div class="d-flex" >
+                                                                <img class="company-logo" src="@/assets/images/cork-logo.png" alt="company" />
+                                                                <h3 class="in-heading align-self-center">DFKJLKFJDSLKF</h3>
+                                                            </div>
+                                                        </div>
+
+                                                        <div class="col-sm-6 align-self-center mt-3" >
+                                                            <p class="inv-street-addr">Estado de Situación Financiera</p>
+                                                            <p class="inv-email-address">SLKADJKLASJDLKASJ</p>
+                                                            <!-- <p class="inv-email-address">(120) 456 789</p> -->
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                                <div class="inv--detail-section inv--customer-detail-section">
+                                                    <div class="row">
+                                                         <div class="col-xl-8 col-lg-7 col-md-6 col-sm-4 align-self-center">
+                                                            <p class="inv-to">ACTIVO</p>
+                                                        </div>
+
+                                                        <div class="col-xl-4 col-lg-5 col-md-6 col-sm-8 align-self-center order-sm-0 order-1 inv--payment-info">
+                                                            <h6 class="inv-title">Payment Info:</h6>
+                                                        </div>
+
+                                                        <div class="col-xl-8 col-lg-7 col-md-6 col-sm-4">
+                                                            <p class="inv-to" style="text-decoration: underline">Activo Corriente</p>
+                                                            <p class="inv-street-addr">Efectivo en caja</p>
+                                                            <p class="inv-email-address">Cuentas por cobrar</p>
+                                                            <p class="inv-email-address">Inventario</p>
+                                                            <p class="inv-to">Total Activos Corrientes</p>
+                                                        </div>
+
+                                                        <div class="col-xl-4 col-lg-5 col-md-6 col-sm-8 col-12 order-sm-0 order-1">
+                                                            <div class="inv--payment-info">
+                                                                <p><span class="inv-subtitle">Bank Name:</span> <span>Bank of America</span></p>
+                                                                <p><span class="inv-subtitle"> </span> <span> </span></p>
+                                                                <p><span class="inv-subtitle">SWIFT code:</span> <span>VS70134</span></p>
+                                                                <p><span class="inv-subtitle">Country: </span> <span>United States</span></p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                                <div class="inv--detail-section inv--customer-detail-section">
+                                                    <div class="row">
+                                                         <div class="col-xl-8 col-lg-7 col-md-6 col-sm-4 align-self-center">
+                                                            <p class="inv-to">PASIVO</p>
+                                                        </div>
+
+                                                        <div class="col-xl-4 col-lg-5 col-md-6 col-sm-8 align-self-center order-sm-0 order-1 inv--payment-info">
+                                                            <h6 class="inv-title">Payment Info:</h6>
+                                                        </div>
+
+                                                        <div class="col-xl-8 col-lg-7 col-md-6 col-sm-4">
+                                                            <p class="inv-to" style="text-decoration: underline">Pasivos Corrientes</p>
+                                                            <p class="inv-street-addr">Cuentas por pagar</p>
+                                                            <p class="inv-email-address">Prestamos a corto plazo</p>
+                                                            <p class="inv-email-address">Prestamos a largo plazo</p>
+                                                            <p class="inv-to">Total Pasivos Corrientes</p>
+                                                        </div>
+
+                                                        <div class="col-xl-4 col-lg-5 col-md-6 col-sm-8 col-12 order-sm-0 order-1">
+                                                            <div class="inv--payment-info">
+                                                                <p><span class="inv-subtitle">Bank Name:</span> <span>Bank of America</span></p>
+                                                                <p><span class="inv-subtitle">Account Number: </span> <span>1234567890</span></p>
+                                                                <p><span class="inv-subtitle">SWIFT code:</span> <span>VS70134</span></p>
+                                                                <p><span class="inv-subtitle">Country: </span> <span>United States</span></p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                                <div class="inv--detail-section inv--customer-detail-section">
+                                                    <div class="row">
+                                                         <div class="col-xl-8 col-lg-7 col-md-6 col-sm-4 align-self-center">
+                                                            <p class="inv-to">PATRIMONIO</p>
+                                                        </div>
+
+                                                        <div class="col-xl-4 col-lg-5 col-md-6 col-sm-8 align-self-center order-sm-0 order-1 inv--payment-info">
+                                                            <h6 class="inv-title">Payment Info:</h6>
+                                                        </div>
+
+                                                        <div class="col-xl-8 col-lg-7 col-md-6 col-sm-4">
+                                                            <p class="inv-street-addr">Capital</p>
+                                                            <p class="inv-to">Total Patrimonio</p>
+                                                        </div>
+
+                                                        <div class="col-xl-4 col-lg-5 col-md-6 col-sm-8 col-12 order-sm-0 order-1">
+                                                            <div class="inv--payment-info">
+                                                                <p><span class="inv-subtitle">Bank Name:</span> <span>Bank of America</span></p>
+                                                                <p><span class="inv-subtitle">Account Number: </span> <span>1234567890</span></p>
+                                                                <p><span class="inv-subtitle">SWIFT code:</span> <span>VS70134</span></p>
+                                                                <p><span class="inv-subtitle">Country: </span> <span>United States</span></p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                                <div class="inv--note">
+                                                    <div class="row mt-4">
+                                                        <div class="col-sm-12 col-12 order-sm-0 order-1">
+                                                            <p>Gracias por usar Capital Quick</p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-xl-3">
+                            <div class="invoice-actions-btn">
+                                <div class="invoice-action-btn">
+                                    <div class="row">
+                                        <div class="col-xl-12 col-md-3 col-sm-6">
+                                            <a href="javascript:;" class="btn btn-primary btn-send">Enviar</a>
+                                        </div>
+                                        <div class="col-xl-12 col-md-3 col-sm-6">
+                                            <a href="javascript:;" class="btn btn-secondary btn-print action-print" @click="print()">Imprimir</a>
+                                        </div>
+                                        <div class="col-xl-12 col-md-3 col-sm-6">
+                                            <a href="javascript:;" class="btn btn-success btn-download">Descargar</a>
+                                        </div>
+                                        <!-- <div class="col-xl-12 col-md-3 col-sm-6">
+                                            <router-link to="/apps/invoice/edit" class="btn btn-dark btn-edit">Edit</router-link>
+                                        </div> -->
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import { onMounted, ref } from 'vue';
+    import '@/assets/sass/apps/invoice-preview.scss';
+
+    import { useMeta } from '@/composables/use-meta';
+    useMeta({ title: 'Invoice Preview' });
+
+    const items = ref([]);
+    const columns = ref([]);
+
+    onMounted(() => {
+        bind_data();
+    });
+
+    const bind_data = () => {
+        columns.value = [
+            { key: 'id', label: 'S.NO' },
+            { key: 'title', label: 'ITEMS' },
+            { key: 'quantity', label: 'QTY' },
+            { key: 'price', label: 'PRICE', class: 'text-end' },
+            { key: 'amount', label: 'AMOUNT', class: 'text-end' },
+        ];
+        items.value = [
+            { id: 1, title: 'Calendar App Customization', quantity: 1, price: '120', amount: '120' },
+            { id: 2, title: 'Chat App Customization', quantity: 1, price: '230', amount: '230' },
+            { id: 3, title: 'Laravel Integration', quantity: 1, price: '405', amount: '405' },
+            { id: 4, title: 'Backend UI Design', quantity: 1, price: '2500', amount: '2500' },
+        ];
+    };
+
+    const print = () => {
+        window.print();
+    };
+</script>
